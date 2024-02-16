82ed9d52f39b1f3b9cf256f9bde7868d06b97ec0
Jeff <yujunglin88@gmail.com>
2024-02-16 19:12:39 +1300

    node backend + react frontend init setup

--- clc_wavetank_sensor/server/index.js
+++ clc_wavetank_sensor/server/index.js
@@ -1,20 +1,90 @@
 const express = require("express");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(cors());
+
+const fluid_density = 997.77; // kg/m^3 (approximate density of water)
+const gravity  = 9.81;
+
+const header_tick = 3;
+var tick = 0;
+
+
+//calculate water depth
+function calculate_water_depth(pressure){
+  return pressure / (fluid_density * gravity);
+}
+
+
+
+//calculate average pressure
+function calculate_average_pressure(pressure_data){
+  return pressure_data.reduce((a, b) => a + b, 0) / pressure_data.length;
+}
+
+
 
 // read in arduino data from COM4
 const {SerialPort} = require('serialport');
 var port = new SerialPort({ path: 'COM4', baudRate: 9600 });
-const { ByteLengthParser } = require('@serialport/parser-byte-length');
+const { ReadlineParser  } = require('@serialport/parser-readline');
 
-const parser = port.pipe(new ByteLengthParser({length: 8}));
-parser.on('data', console.log);
+//pressure data array
+var pressure_data = [];
+var water_depth = [];
+var average_pressure;
 
+const parser = port.pipe(new ReadlineParser());
+// convert buffer to string
+parser.on('data', function(data){
+    if (tick++ < header_tick) return;
+
+    console.log(data.toString());
+    // add current tick to the pressure data
+    pressure_data.push(data.toString());
+    
+    //calculate current water depth and push to water depth array
+    water_depth.push(calculate_water_depth(data.toString()));
+    
+    //calculate average pressure and set average pressure to current pressure
+    average_pressure = calculate_average_pressure(pressure_data);
+
+});
+
+
+
+
+
+// handle error event
+parser.on('error', function(err){
+    console.log('Error: ', err.message);
+});
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    //send the last 10 pressure data
+    res.json({ 
+        tick: tick,
+        // array[:-50]: pressure data
+        pressure_data: pressure_data.slice(-50),
+        // array[:-50]: water depth
+        water_depth: water_depth.slice(-50),
+        // average pressure
+        average_pressure: average_pressure
+
+        // average water depth
+        
+        // max pressure
+        // max wave height
+
+
+      });
+
+
+        
+    
 });
 
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,60 @@
 import React from "react";
 import logo from "./logo.svg";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import "./App.css";
+
+
 
 function App() {
   const [data, setData] = React.useState(null);
 
+  const test_data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+                  {name: 'Page B', uv: 3000,pv: 1398,amt: 2210,},
+                  {name: 'Page C',uv: 2000,pv: 9800,amt: 2290,},
+                  {name: 'Page D',uv: 2780,pv: 3908,amt: 2000,},
+                  {name: 'Page E',uv: 1890,pv: 4800,amt: 2181,},
+                  {name: 'Page F',uv: 2390,pv: 3800,amt: 2500,},
+                  {name: 'Page G',uv: 3490,pv: 4300,amt: 2100}
+  ];
+  
+
+
+                  
+  const renderLineChart = (
+    <LineChart width={600} height={300} data={test_data}>
+      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" />
+      <XAxis dataKey="name" />
+      <YAxis />
+    </LineChart>
+  );
+
+
+  // listen to the server to continuously get the latest pressure data
   React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+    const interval = setInterval(() => {
+      try {
+        fetch("/api")
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data.pressure_data.join(", "));
+        });
+      } catch (error) {
+          // no need to update the state if the fetch fails
+      }
+      
+    }, 30);
+    return () => clearInterval(interval);
   }, []);
+
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>{!data ? "Loading..." : data}</p> */}
+        <p>{renderLineChart}</p>
       </header>
     </div>
   );
